50f76840ff88be12d3b99431257eb54a2fd5e60f
phphe <phphe@outlook.com>
2021-12-14 23:00:27 +0800

    add example url '/hello'

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,11 @@
-import { FastifyInstance } from 'fastify'
-import aws_s3 from './aws_s3'
+import { FastifyInstance } from "fastify";
+import aws_s3 from "./aws_s3";
 
-const prefix = '/'
+const prefix = "/";
 
 export const initAPI = (app: FastifyInstance) => {
-  app.register(aws_s3, { prefix: prefix })
-}
+  app.get("/hello", (req, reply) => {
+    reply.send("Hello world");
+  });
+  app.register(aws_s3, { prefix: prefix });
+};
